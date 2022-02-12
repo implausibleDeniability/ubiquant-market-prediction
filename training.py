380632ec2272488cd0017539688dea15264fa492
Maxim Faleev <m.faleev@innopolis.university>
2022-02-12 20:50:36 +0000

    + training script (acceptable design)

--- training.py
+++ training.py
@@ -1,87 +1,131 @@
-#!/usr/bin/env python
-# See the current state of R&D in [Notion](https://www.notion.so/MLP-0f9b2dbf6013405385eac14638a9a587).
-
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-import pandas as pd
-import numpy as np
-from scipy import stats
 
+import numpy as np
+import pandas as pd
 import torch
+from dotenv import load_dotenv
+from scipy import stats
 from torch import nn
 from torch.utils.data import DataLoader
-import torch.optim.lr_scheduler as lr_scheduler
+from tqdm import tqdm
 
-from src.metrics import pearson_metric
-from src.torch_models import EmbedMLP, MLP
+import wandb
 from src.data import Dataset
-
-load_dotenv()
+from src.metrics import pearson_metric
+from src.torch_models import EmbedMLP
 
 # configs
-
+load_dotenv()
 device = "cuda"
-dataset_dir = Path(os.environ['dataset_dir'])
+data_sample_frac = 1.0
 
 # hyperparameters
+batch_size = 25000
+epochs = 100
 investment_id_dropout = 0.01
-lr = 9e-4
-epochs = 30
+lr = 1e-3
 
-# load data
-data = pd.read_csv(dataset_dir / 'train.csv')
-data = data.sample(frac=0.01)
-data = data.set_index('row_id')
-data.investment_id += 1
 
-# prepare train test
-train = data[data.time_id < 1000]
-test = data.query("1000 <= time_id")
-learned_investments = train.investment_id.unique()
-new_investments_in_test = test.query("investment_id not in @learned_investments").index
-test.loc[new_investments_in_test].investment_id = 0
+def main():
+    data = load_data()
+    train, test = make_splits(data)
+    train, test = process_investment_id(train, test)
+    train, test = train.drop("time_id", axis=1), test.drop("time_id", axis=1)
 
-train_dataset = Dataset(train.drop("time_id", axis=1))
-test_dataset = Dataset(test.drop("time_id", axis=1))
-model = EmbedMLP(input_dim=301, hidden_dim=90, depth=3, activation=nn.ReLU).to(device)
-train_dataloader = DataLoader(train_dataset, batch_size=24500)
-test_dataloader = DataLoader(test_dataset, batch_size=24500)
+    train_dataloader = DataLoader(Dataset(train), batch_size=batch_size)
+    test_dataloader = DataLoader(Dataset(test), batch_size=batch_size)
+    model = EmbedMLP(input_dim=301, num_embeddings=3775).to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        optimizer, max_lr=0.01, epochs=epochs, steps_per_epoch=len(train_dataloader)
+    )
+    wandb.init(project="market_prediction", entity="parmezano", name="training_script")
+    train_model(model, train_dataloader, test_dataloader, optimizer, scheduler)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
-scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer,
-                                                max_lr=0.026,
-                                                epochs=epochs,
-                                                steps_per_epoch=len(train_dataloader)                            
-            )
-loss_function = nn.MSELoss()
-for i in range(epochs):
+
+def load_data() -> pd.DataFrame:
+    dataset_dir = Path(os.environ["dataset_dir"])
+    data = pd.read_csv(dataset_dir / "train.csv")
+    data = data.set_index("row_id")
+    data = data.sample(
+        frac=data_sample_frac
+    )  # because full data doesn't fit into memory
+    # investment ids start from 0, but I need the 0th investment to be "unseen" investment
+    # that we will use if the new (unseen) investment in the test set arises
+    data.investment_id += 1
+    return data
+
+
+def make_splits(data: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    # train on earlier data, test on later data
+    train = data.query("time_id < 1000")
+    test = data.query("time_id >= 1000")
+    return train, test
+
+
+def process_investment_id(
+    train: pd.DataFrame, test: pd.DataFrame
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    learned_investments = train.investment_id.unique()
+    new_investments_in_test = test.query(
+        "investment_id not in @learned_investments"
+    ).index
+    test.loc[new_investments_in_test].investment_id = 0
+    return train, test
+
+
+def make_datasets(train: pd.DataFrame, test: pd.DataFrame):
+    train_dataset = Dataset(train.drop("time_id", axis=1))
+    test_dataset = Dataset(test.drop("time_id", axis=1))
+    return train_dataset, test_dataset
+
+
+def train_epoch(model, dataloader, optimizer, scheduler, criterion) -> list[float]:
     model.train()
-    train_loss = 0
-    for batch in train_dataloader:
+    losses = []
+    for batch in tqdm(dataloader):
         optimizer.zero_grad()
+
         x, y_true = batch
-
-        x[:, 0] *= (torch.rand(len(x)) > investment_id_dropout)
-
+        x[:, 0] *= torch.rand(len(x)) > investment_id_dropout
         x = x.to(device)
         y_true = y_true.to(device)
-        
+
         y_pred = model(x)
-        loss = loss_function(y_true, y_pred.view(-1))
+        loss = criterion(y_true, y_pred.view(-1))
         loss.backward()
         optimizer.step()
-        train_loss += loss.item()
-        scheduler.step()
-        # TODO: assess by Pearson coefficient
-    print(f"EPOCH {i + 1}. Train loss: {train_loss/len(train)}")
+        losses.append(loss.item())
+    return losses
 
+
+def eval_epoch_loss(model, dataloader, criterion):
     model.eval()
-    test_loss = 0
+    losses = []
     with torch.no_grad():
-        for batch in test_dataloader:
+        for batch in tqdm(dataloader):
             x, y_true = batch[0].to(device), batch[1].to(device)
             y_pred = model(x)
-            loss = loss_function(y_true, y_pred.view(-1))
-            test_loss += loss.item()
-    print(f"EPOCH {i + 1}. Test loss: {test_loss/len(test)}")
+            loss = criterion(y_true, y_pred.view(-1))
+            losses.append(loss.item())
+    return losses
+
+
+def train_model(model, train_dataloader, test_dataloader, optimizer, scheduler):
+    criterion = nn.MSELoss()
+    for i in range(epochs):
+        train_losses = train_epoch(
+            model, train_dataloader, optimizer, scheduler, criterion
+        )
+        test_losses = eval_epoch_loss(model, test_dataloader, criterion)
+        wandb.log(
+            {
+                "train_loss": np.mean(train_losses, axis=0),
+                "test_loss": np.mean(test_losses, axis=0),
+            }
+        )
+
+
+if __name__ == "__main__":
+    main()
