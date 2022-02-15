ef6f3894fbc7f49a02842221d48f91d76a5d257a
Maxim Faleev <m.faleev@innopolis.university>
2022-02-15 09:46:29 +0000

    + correct pd2np conversion

--- src/data.py
+++ src/data.py
@@ -3,9 +3,9 @@ import numpy as np
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, data):
-        self.x = data.drop('target', axis=1).values.astype(np.float32)
-        self.y = data['target'].values.astype(np.float32)
-        
+        self.x = data.drop("target", axis=1).to_numpy(dtype=np.float32)
+        self.y = data["target"].values.astype(np.float32)
+
     def __getitem__(self, idx):
         return self.x[idx], self.y[idx]
     
