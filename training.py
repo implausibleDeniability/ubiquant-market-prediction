380657378f522472cd98e074e2c0f30f87621cb9
Maxim Faleev <m.faleev@innopolis.university>
2022-02-15 18:59:14 +0000

    configured input from configs; + cpu run

--- training.py
+++ training.py
@@ -6,6 +6,7 @@ import click
 import numpy as np
 import pandas as pd
 import torch
+import yaml
 from dotenv import load_dotenv
 from scipy import stats
 from torch import nn
@@ -20,38 +21,36 @@ from src.weights_logger import WeightsLogger
 
 # configs
 load_dotenv()
-device = "cuda"
-data_sample_frac = 1.0
-
-# hyperparameters
-batch_size = 25000
-epochs = 15
-investment_id_dropout = 0.01
-lr = 1e-3
 
 
 @click.command()
 @click.option("--exp-name", type=str, default="unnamed_run", help="Name of experiment")
+@click.option("--config", type=str, default="base", help="Training configuration")
 @click.option("--debug", is_flag=True, default=False)
-def main(exp_name: str, debug: bool):
-    if debug:
-        wandb_mode = "disabled"
-    else:
-        wandb_mode = "online"
+@click.option("--cpu", is_flag=True, default=False)
+def main(exp_name: str, config: str, debug: bool, cpu: bool):
+    wandb_mode = "disabled" if debug else "online"
+    wandb.init(
+        project="market_prediction", entity="parmezano", name=exp_name, mode=wandb_mode
+    )
+    with open(f"configs/{config}.yaml", "r") as stream:
+        wandb.config = yaml.safe_load(stream)
+    wandb.config["device"] = "cpu" if cpu else "cuda"
+
     data = load_data(debug=debug)
     train, test = make_splits(data)
     train, test = process_investment_id(train, test)
     train, test = train.drop("time_id", axis=1), test.drop("time_id", axis=1)
 
-    train_dataloader = DataLoader(Dataset(train), batch_size=batch_size)
-    test_dataloader = DataLoader(Dataset(test), batch_size=batch_size)
-    model = EmbedMLP(input_dim=301, num_embeddings=3775).to(device)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+    train_dataloader = DataLoader(Dataset(train), batch_size=wandb.config["batch_size"])
+    test_dataloader = DataLoader(Dataset(test), batch_size=wandb.config["batch_size"])
+    model = EmbedMLP(input_dim=301, num_embeddings=3775).to(wandb.config["device"])
+    optimizer = torch.optim.AdamW(model.parameters(), lr=wandb.config["lr"])
     scheduler = torch.optim.lr_scheduler.OneCycleLR(
-        optimizer, max_lr=0.01, epochs=epochs, steps_per_epoch=len(train_dataloader)
-    )
-    wandb.init(
-        project="market_prediction", entity="parmezano", name=exp_name, mode=wandb_mode
+        optimizer,
+        max_lr=0.01,
+        epochs=wandb.config["epochs"],
+        steps_per_epoch=len(train_dataloader),
     )
     weights_logger = WeightsLogger(os.environ["weights_dir"], exp_name, debug)
     train_model(
@@ -67,7 +66,7 @@ def load_data(debug=False) -> pd.DataFrame:
         data = pd.read_csv(dataset_dir / "train.csv")
     data = data.set_index("row_id")
     data = data.sample(
-        frac=data_sample_frac
+        frac=wandb.config["data_sample_frac"]
     )  # because full data doesn't fit into memory
     # investment ids start from 0, but I need the 0th investment to be "unseen" investment
     # that we will use if the new (unseen) investment in the test set arises
@@ -102,13 +101,14 @@ def make_datasets(train: pd.DataFrame, test: pd.DataFrame):
 def train_epoch(model, dataloader, optimizer, scheduler, criterion) -> list[float]:
     model.train()
     losses = []
+    investment_id_dropout = wandb.config["investment_id_dropout"]
     for batch in tqdm(dataloader):
         optimizer.zero_grad()
 
         x, y_true = batch
         x[:, 0] *= torch.rand(len(x)) > investment_id_dropout
-        x = x.to(device)
-        y_true = y_true.to(device)
+        x = x.to(wandb.config["device"])
+        y_true = y_true.to(wandb.config["device"])
 
         y_pred = model(x)
         loss = criterion(y_true, y_pred.view(-1))
@@ -123,7 +123,8 @@ def eval_epoch_loss(model, dataloader, criterion):
     losses = []
     with torch.no_grad():
         for batch in tqdm(dataloader):
-            x, y_true = batch[0].to(device), batch[1].to(device)
+            x = batch[0].to(wandb.config["device"])
+            y_true = batch[1].to(wandb.config["device"])
             y_pred = model(x)
             loss = criterion(y_true, y_pred.view(-1))
             losses.append(loss.item())
@@ -134,7 +135,7 @@ def train_model(
     model, train_dataloader, test_dataloader, optimizer, scheduler, weights_logger
 ) -> None:
     criterion = nn.MSELoss()
-    for i in range(epochs):
+    for i in range(wandb.config["epochs"]):
         train_losses = train_epoch(
             model, train_dataloader, optimizer, scheduler, criterion
         )
