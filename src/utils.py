f401af64ef719ce83856917210567435cc0c144f
Maxim Faleev <m.faleev@innopolis.university>
2022-02-19 19:44:11 +0000

    modified utils file organization

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,12 @@
 import os
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 from dotenv import load_dotenv
 
-load_dotenv()
+def make_sample():
+    load_dotenv()
 
-dataset_dir = Path(os.environ["dataset_dir"])
-data = pd.read_csv(dataset_dir / "train.csv")
-data.sample(frac=0.01).to_csv(dataset_dir / "train_sample.csv")
+    dataset_dir = Path(os.environ["dataset_dir"])
+    data = pd.read_csv(dataset_dir / "train.csv")
+    data.sample(frac=0.01).to_csv(dataset_dir / "train_sample.csv")
