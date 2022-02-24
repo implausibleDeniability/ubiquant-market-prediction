5b1f4bfd2712cf78015d39046a8d15ae501e74a3
danis <aldanis73@yandex.ru>
2022-02-25 00:09:34 +0300

    + feather load, MLP+AE

--- src/data.py
+++ src/data.py
@@ -1,6 +1,10 @@
+import os 
+import time
 import torch
 import numpy as np
-
+from torch.utils.data import DataLoader
+from pathlib import Path
+import pandas as pd
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, data):
@@ -12,3 +16,45 @@ class Dataset(torch.utils.data.Dataset):
 
     def __len__(self,):
         return len(self.x)
+
+    
+def load_data(use_feather=False, split_data=True, batch_size=25000):
+    """
+    Method used to construct dataloader for Ubiquant train dataset.
+    Can use feather format to optimize the loading: ~176.26 s. vs ~9.30 s.
+    for .csv vs .feather respectfully.
+    :param use_feather: flag to use optimized loading
+    :param split_data: flag to split the data into train and test (ids > 1000)
+    :param batch_size: batch size 
+    :return: train, test loader (if not split_data returns None)
+    """
+    start_execution = time.time()
+    csv_file = 'train.csv'
+    feather_file = 'train32.feather'
+    
+    dataset_dir = Path(os.environ['dataset_dir'])
+    if use_feather:
+        data = pd.read_feather(dataset_dir / feather_file)
+    else:
+        data = pd.read_csv(dataset_dir / csv_file)
+        data = data.set_index('row_id')
+    data.investment_id += 1
+    
+    if split_data:
+        train = data[data.time_id < 1000]
+        test = data.query("1000 <= time_id")
+        learned_investments = train.investment_id.unique()
+        new_investments_in_test = test.query("investment_id not in @learned_investments").index
+        test.loc[new_investments_in_test].investment_id = 0
+        
+        trainset = Dataset(train.drop("time_id", axis=1))
+        testset = Dataset(test.drop("time_id", axis=1))
+
+        testloader = DataLoader(testset, batch_size=batch_size)        
+    else:
+        train = data.copy()
+        trainset = Dataset(train.drop("time_id", axis=1))
+        testloader = None
+    trainloader = DataLoader(trainset, batch_size=batch_size)
+    print(f"Loading took {time.time() - start_execution:.2f} seconds")
+    return trainloader, testloader
--- src/torch_models.py
+++ src/torch_models.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from collections import OrderedDict
+from math import log10
 
 
 class MLP(nn.Module):
@@ -96,3 +97,80 @@ class MLPLogExtended(nn.Module):
         x = torch.cat([x, torch.log(x)], dim=1)
         x = self.main(x)
         return x
+
+    
+def closest_power_of_2(n):
+    res = 0;
+    for i in range(n, 0, -1):
+        if ((i & (i - 1)) == 0):
+            res = i
+            break
+    return res    
+
+
+class MLPAE(nn.Module):
+    def __init__(self, input_dim=300, bottleneck_dim=64, hidden_dim=100, mlp_depth=2, activation=nn.ReLU):
+        super(MLPAE, self).__init__()
+        self.input_dim = input_dim
+        assert (bottleneck_dim & (bottleneck_dim-1) == 0) and bottleneck_dim != 0, "Bottleneck dimension should be power of 2"     
+        self.bottleneck_dim = bottleneck_dim
+        self.mlp_depth = mlp_depth
+        self.hidden_dim = hidden_dim
+        self.activation = activation
+        self.ae_depth = int((log10(input_dim) - log10(bottleneck_dim)) / log10(2)) - 1
+        
+        self.ae_encoder = self.__build_encoder()
+        self.ae_decoder = self.__build_decoder()
+        self.mlp = self.__build_mlp()
+    
+    def __build_encoder(self):
+        hidden_dim = closest_power_of_2(self.input_dim)
+        encoder_layers = [nn.Linear(self.input_dim, hidden_dim)]
+        for i in range(self.ae_depth):
+            input_dim = hidden_dim
+            output_dim = int(hidden_dim / 2)
+            layer = BlockAE(input_dim, output_dim, self.activation)
+            encoder_layers.append(layer)
+            hidden_dim = output_dim
+        encoder_layers.append(nn.Linear(hidden_dim, int(hidden_dim / 2)))
+        return torch.nn.Sequential(*encoder_layers)
+    
+    def __build_decoder(self):
+        hidden_dim = self.bottleneck_dim
+        decoder_layers = []
+        for i in range(self.ae_depth):
+            input_dim = hidden_dim
+            output_dim = int(hidden_dim * 2)
+            layer = BlockAE(input_dim, output_dim, self.activation)
+            decoder_layers.append(layer)
+            hidden_dim = output_dim
+        decoder_layers.append(nn.Linear(hidden_dim, int(hidden_dim * 2)))
+        decoder_layers.append(nn.Linear(int(hidden_dim * 2), self.input_dim))
+        return torch.nn.Sequential(*decoder_layers)
+        
+    def __build_mlp(self):
+        mlp_layers = [nn.Linear(self.bottleneck_dim + self.input_dim, self.hidden_dim)]
+        for i in range(self.mlp_depth):
+            mlp_layers.append(ResidualBlock(self.hidden_dim, self.activation, 0.0))
+        mlp_layers.append(nn.Linear(self.hidden_dim, 1))
+        return torch.nn.Sequential(*mlp_layers)
+    
+    def forward(self, x):
+        representation = self.ae_encoder(x)
+        extended = torch.cat((x, representation), 1)
+        predicted = self.mlp(extended)        
+        mapping = self.ae_decoder(representation)
+        return mapping, predicted
+
+        
+class BlockAE(nn.Module):
+    def __init__(self, input_dim, output_dim, activation):
+        super(BlockAE, self).__init__()
+        self.main = nn.Sequential(
+            nn.Linear(input_dim, output_dim),
+            activation()
+        )
+        
+    def forward(self, x):
+        out = self.main(x)
+        return out
