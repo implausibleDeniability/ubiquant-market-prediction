6377e800aa0017eceb7a55b464de8b899bae5e59
danis <aldanis73@yandex.ru>
2022-02-27 01:07:14 +0300

    + export feather

--- src/utils.py
+++ src/utils.py
@@ -10,3 +10,11 @@ def make_sample():
     dataset_dir = Path(os.environ["dataset_dir"])
     data = pd.read_csv(dataset_dir / "train.csv")
     data.sample(frac=0.01).to_csv(dataset_dir / "train_sample.csv")
+
+    
+def convert2feather():
+    load_dotenv()
+    
+    dataset_dir = Path(os.environ["dataset_dir"])
+    data = pd.read_csv(dataset_dir / "train.csv")
+    data.to_feather(dataset_dir / 'train.feather')
